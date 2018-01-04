b1b7a43874129d7547ab887cbbbc05bde1fd6827
damianedwards <damian@damianedwards.com>
2018-01-03 16:54:53 -0800

    Initial commit

---
+++ CookieConsentUI/CookiePolicyOptions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.CookiePolicy
+{
+    public class CookiePolicyOptions
+    {
+        public CookieBuilder ConsentCookie { get; set; } = new CookieBuilder()
+        {
+            Name = ".AspNet.Consent",
+            Expiration = TimeSpan.FromDays(365 * 10),
+            //IsEssential = true,
+        };
+    }
+}
---
+++ CookieConsentUI/ITrackingConsentFeature.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookieConsentUI
+{
+    public interface ITrackingConsentFeature
+    {
+        bool CanTrack { get; }
+    }
+
+    internal class TrackingConsentFeature : ITrackingConsentFeature
+    {
+        private readonly HttpContext _context;
+
+        public TrackingConsentFeature(HttpContext context)
+        {
+            _context = context;
+        }
+
+        public bool CanTrack
+        {
+            get
+            {
+                var cookie = _context.Request.Cookies[".AspNet.Consent"];
+                var hasConsent = string.Equals(cookie, "yes", StringComparison.Ordinal);
+                return hasConsent;
+            }
+        }
+    }
+}
